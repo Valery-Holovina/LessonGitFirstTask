26beb3cd2119dab3ef4c7fda48240ea70ca83bac
Valery-Holovina <valery.holovina09@gmail.com>
2025-01-29 18:25:14 +0200

    Initial Commit

---
+++ lessonGitTask1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lessonGitTask1
+//
+//  Created by Valery on 29.01.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
